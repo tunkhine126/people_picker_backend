7fa751859afbf8c124bcc48d20d0e8a948a2079b
Tun Khine <tunkhine126@gmail.com>
2020-01-14 13:55:05 -0500

    Fixed routes and controller

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,24 +1,22 @@
 class PeopleController < ApplicationController
 
     def index 
-        @peoples = People.all
-        render json: @peoples
-      end
-    
-      def create
-        @people = People.create(people_params)
-        render json: { people: People.create(@people) }, status: :created
-      end
-    
-      def show
-        render json: People.find(params[:id])
-      end
-    
-      private
+      @peoples = People.all
+      render json: @peoples
+    end
+
+    def create
+      @people = People.create(people_params)
+      render json: { people: People.create(@people) }, status: :created
+    end
+
+    def show
+      render json: People.find(params[:id])
+    end
+
+    private
       def people_params
         params.require(:people).permit(:name, :email)
       end
-    
-    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :peoples, only: [:index, :show, :create]
+  resources :people, only: [:index, :show, :create]
 
 end
